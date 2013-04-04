a5ef4daa83c0b1d322e863cba54df81545a7b555
kaibrusch <kaibrusch@googlemail.com>
2013-04-04 15:16:18 +0200

    done A1

--- A1/src/a1/Main.java
+++ A1/src/a1/Main.java
@@ -4,31 +4,27 @@
  */
 package a1;
 
-
 /**
  *
  * @author kbrusch, nitsche
  */
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        //porsche
         Porsche911GT2RS porsche = new Porsche911GT2RS(1445.0, 456.0, 330.0);
-
         porsche.reset();
-        
-        double deltaTime = 1.0;
+
+        double deltaTime = 0.01;
         double level = 1.0;
-        double uptospeed = 100.0;
         
-        
-        for (double i = 0; i < uptospeed; i++){
-            porsche.step(deltaTime+=2.8, level);
-            System.out.println("Time: "+porsche.getTime());
-            System.out.println("Pos: "+porsche.getPos());
-            System.out.println("Speed: "+porsche.getSpeed());
+        int iter = 20000;
+        for (double i = 0; i < iter; i++) {
+
+            porsche.step(deltaTime, level);
+            System.out.println(porsche.toString());
+            
         }
-    System.out.println("deltaTime: "+deltaTime);
+        
     }
 }
--- A1/src/a1/Porsche911GT2RS.java
+++ A1/src/a1/Porsche911GT2RS.java
@@ -10,18 +10,25 @@ package a1;
  */
 public class Porsche911GT2RS {
 
-    private double mass;
-    private double powerPropMax;
-    private double speedMax;
-    private double speed;
-    private double level;
-    private double pos;
-    private double time;
+    // 
+    private double mass; // KG
+    private double powerPropMax; // KW -> (kg*m^2)/s^3
+    private double speedMax; // MS
+    private double speed; // MS
+    private double level; // 
+    private double pos; //
+    private double time; //
+    
+    // Class Constants
+    private static final double ACC_EARTH = 9.81; // M / S²
+    private static final double KMH_IN_MS = (1/3.6);
+    private static final double KILO = 1000;
+   
 
     public Porsche911GT2RS(double mass, double powerPropMax, double speedMax) {
-        this.mass = mass;
-        this.powerPropMax = powerPropMax;
-        this.speedMax = speedMax;
+        this.mass = mass; // KG
+        this.powerPropMax = powerPropMax*KILO;
+        this.speedMax = speedMax*KMH_IN_MS;
     }
 
     public void set(double time, double pos, double speed, double level) {
@@ -35,30 +42,44 @@ public class Porsche911GT2RS {
         set(0.0, 0.0, 0.0, 0.0);
     }
 
+    @Override
+    public String toString() {
+        return "Time: " + this.time
+                + " Position: " + this.pos
+                + " Speed: " + this.speed/KMH_IN_MS;
+    }
+
     public void step(double deltaTime, double level) {
 
-        double accEarth = 9.81;
+        double powerProp; //
+        double forcePropMax; // kg*m*s^-2
+        double forcePropAbs; // kg*m*s^-2
+        double forceProp; // kg*m*s^-2
+        double dragConst; // kg/m
+        double forceDrag; // kg*m*s^-2
+        double force; // kg*m*s^-2
+        double acc; // m/s^2
+        
         this.level = level;
 
+        // Dynamik
 
+        powerProp = Math.abs(this.level) * this.powerPropMax;
+        // Newton (kg*m/s²) = kg * m/s²
+        forcePropMax = this.mass * ACC_EARTH;
+        // Newton (kg*m/s²) = 
+        forcePropAbs = Math.min(forcePropMax, powerProp / Math.abs(this.speed));
+        forceProp = forcePropAbs * Math.signum(this.level);
+        dragConst = Math.abs(powerProp / (Math.pow(this.speedMax, 3.0)));
+        forceDrag = dragConst * Math.pow(this.speed, 2.0) * Math.signum(-(this.speed));
+        force = forceProp + forceDrag;
 
-        //dynamik
+        // Kinematik
 
-        double powerProp = Math.abs(this.level) * this.powerPropMax;
-        double forcePropMax = this.mass * accEarth;
-        double forcePropAbs = Math.min(forcePropMax, powerProp / Math.abs(this.speed));
-        double forceProp = forcePropAbs * Math.signum(this.level);
-        double dragConst = Math.abs(powerProp / (Math.pow(this.speedMax, 3.0)));
-        double forceDrag = dragConst * Math.pow(this.speed, 2.0) * Math.signum(-(this.speed));
-        double force = forceProp + forceDrag;
-
-        //Kinematik
         this.pos = this.pos + (this.speed * deltaTime);
-        this.time = (this.time + deltaTime);
-        double acc = force / this.mass;
+        acc = force / this.mass;
         this.speed = this.speed + (acc * deltaTime);
-
-
+        this.time = (this.time + deltaTime);
     }
 
     public double getTime() {
