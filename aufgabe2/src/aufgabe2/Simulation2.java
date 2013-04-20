f4a2cde21955a6ed174dee2cbf00e5c15ec06cc0
sacry- <Nitsche25@gmail.com>
2013-04-20 17:11:46 +0200

    simulation extended

--- aufgabe2/src/aufgabe2/Simulation2.java
+++ aufgabe2/src/aufgabe2/Simulation2.java
@@ -4,6 +4,8 @@
  */
 package aufgabe2;
 
+import jgame.JGColor;
+import jgame.JGObject;
 import jgame.JGPoint;
 import jgame.platform.JGEngine;
 
@@ -14,16 +16,28 @@ import jgame.platform.JGEngine;
 public class Simulation2 extends JGEngine {
 
     private Porsche911 porsche;
-    
+
+    public Simulation2() {
+        this.initEngineApplet();
+    }
+
     public Simulation2(Porsche911 p, JGPoint jgPoint) {
+        super();
         this.porsche = p;
+        this.initEngine(jgPoint.x, jgPoint.y);
     }
 
     @Override
     public void initCanvas() {
+        this.setCanvasSettings(800, 400, 1, 1, JGColor.yellow, JGColor.yellow, null);
     }
 
     @Override
     public void initGame() {
+        setFrameRate(35, 2);
+        JGObject jgob = new JGObject("Background", true, 0, 0, 1, "background");
+        porsche.reset();
+        setGameState("Wait");
     }
+    
 }
