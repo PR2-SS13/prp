dfef0cbba8f1ebe792efa3f8b2254dd95e39ee60
kaibrusch <kaibrusch@googlemail.com>
2013-04-20 17:15:06 +0200

    awt toolkit fix

--- aufgabe2/src/aufgabe2/Main.java
+++ aufgabe2/src/aufgabe2/Main.java
@@ -12,8 +12,8 @@ import jgame.JGPoint;
  * @author kbrusch
  */
 public class Main {
-    
-        /**
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -21,6 +21,4 @@ public class Main {
         Porsche911 p = new Porsche911(1445.0, 456.0, 330.0);
         Simulation2 simulation = new Simulation2(p, new JGPoint(o.width / 2, o.height / 2));
     }
-    
-    
 }
