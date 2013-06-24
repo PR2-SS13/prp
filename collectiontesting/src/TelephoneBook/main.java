88c69bc2d7b35202caa59310e6f28c4431c2bf52
sacry- <nitsche25@gmail.com>
2013-06-24 11:37:24 +0200

    Telephone Book

--- collectiontesting/src/TelephoneBook/main.java
+++ collectiontesting/src/TelephoneBook/main.java
@@ -17,19 +17,19 @@ public class main {
         Telephone t1 = new Telephone("Germany");
         ab.add(t1);
 
-        Address adr1 = new Address("Germany", "Hamburg", 22085, "Herderstraße", 77);
-        Entry e1 = new Entry("Matthias", "Nitsche", "017680210824", adr1);
+        Address adr1 = new Address("Germany", "Hamburg", 123456, "w00tstreet", 67);
+        Entry e1 = new Entry("Matze", "w00t", "123456", adr1);
         t1.add(e1);
 
-        Address adr2 = new Address("Germany", "Hamburg", 123456, "Harburgerstraße", 12);
-        Entry e2 = new Entry("Kai", "Brusch", "0156782910", adr2);
+        Address adr2 = new Address("Germany", "Hamburg", 123456, "w00tstreet", 12);
+        Entry e2 = new Entry("Kai", "lols", "123456", adr2);
         t1.add(e2);
         
         Telephone t2 = new Telephone("Russia");
         ab.add(t2);
 
         Address adr3 = new Address("Russia", "Moscow", 43564, "Vladivostocstreet", 4);
-        Entry e3 = new Entry("Ivan", "chock", "017680210824", adr3);
+        Entry e3 = new Entry("Ivan", "Abakumov", "123456", adr3);
         t2.add(e3);
 
         Address adr4 = new Address("Russia", "Moscow", 43564, "Vladivostocstreet", 7);
